bdd7b4651d68ac8461d2a34035c2693b732d165a
DIVYANSHU1210 <84267089+DIVYANSHU1210@users.noreply.github.com>
2023-09-11 16:20:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,30 @@
-//your code here
+let arr = ['The Virupaksha Temple', 'a Victoria Memorial', 'Tajmahal'];
 
+
+// let newspots = spots.Replace(/\bthe\b|\wan\b|\ba\b/gi , "");
+// consol.log(newspots);
+// console.log(str.match(/\bthe\b|\wan\b|\ba\b/g));
+
+
+
+// let str = "an rockey launched a the rocket";
+// console.log(str);
+// console.log(str.match(/\bthe\b|\ban\b|\ba\b/g));
+let newele = [];
+
+let mp = {}
+
+for(let i=0; i<arr.length; i++){
+    let ele = arr[i].replace(/\bthe\b|\ban\b|\ba\b/gi, "").trim();
+    newele.push(ele);
+    mp[ele] = arr[i];
+}
+
+newele.sort();
+
+let ans = [];
+
+for(let i of newele){
+    ans.push(mp[i]);
+}
+console.log(ans);
